1d0e579042adf3f35fc622156eb66494bee5f706
start.vaadin.com <no-reply@vaadin.com>
2022-04-21 22:32:35 +0000

    Generated project

---
+++ src/main/java/com/simreal/data/service/SamplePersonRepository.java
@@ -0,0 +1,9 @@
+package com.simreal.data.service;
+
+import com.simreal.data.entity.SamplePerson;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SamplePersonRepository extends JpaRepository<SamplePerson, UUID> {
+
+}
